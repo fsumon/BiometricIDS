b58c899b9eb12eef0a2bd868783e88b273f65713
Farhad <56811314+fsumon@users.noreply.github.com>
2021-09-28 00:13:07 +0930

    Add files via upload
    ECGPPG scripts

--- featureExtraction.m
+++ featureExtraction.m
@@ -1,76 +1,90 @@
-%function [trainFeatures,featureindices] = featureExtraction(trainData,T,AR_order,level)
-function [trainFeatures,featureindices] = featureExtraction(trainData,T,AR_order,level)
-% This function is only in support of XpwWaveletMLExample. It may change or
-% be removed in a future release.
-trainFeatures = [];
+%function [trainFeatures, testFeatures,featureIndices] = featureExtraction(trainData,testData,T,AROrder,level)
+%function [allFeatures,featureIndices] = featureExtraction(TrainTestData,T,AROrder,level)
 
 
-for idx =1:size(trainData,1)
-    x = trainData(idx,:);
-    x = detrend(x,0);
-    arcoefs = blockAR(x,AR_order,T);
-    se = shannonEntropy(x,T,level);
-    %le=leaders(x,T);
+function [testFeatures,featureindices] = featureExtraction(testData1,T,AROrder,level)
 
-    % [cp,rh] = leaders(x,T);
- 
-      wvar = modwtvar(modwt(x,'haar'),'haar'); % 85.5%
-      
-      sigenergy = sum(x.^2,2);
-      absvalue=abs(x);
-      %wvar1 = modwtvar(modwpt(x,'haar'),'haar'); % 85.5%
-      %wvar2 = modwtvar(modwt(x,'haar'),'haar'); % 85.5%
-     
-   % wvar = modwtvar(modwt(y,'db15'),'db15'); % 82%
-   % wvar = modwtvar(modwt(y,'db4'),'db4');
-    
-    %wvar = modwtvar(modwt(y,'coif1'),'coif1');
-    %wvar = modwtvar(modwt(y,'fk22'),'fk22');
-    %wvar = modwtvar(modwt(x,'sym2'),'sym2');
-    sep = pentropy(x,T);
-    ifq=  ceil(instfreq(x,T));
-    ps= ceil(pspectrum(x,T));
-    wp = wpe(x,T,level);
-    
-   
-    %cvar=cwt(x,'bump')
-    wtropy = wentropy(x,'shannon');
-    wtropy1 = wentropy(x,'log energy');
-    
-    %wavelet packet energy
-    
-    
-    [C,L] = wavedec(x,12,'haar');
-    [Ea,Ed] = wenergy(C,L);
-    
-    %trainFeatures = [trainFeatures; arcoefs se wvar']; %#ok<AGROW>
-    %trainFeatures = [trainFeatures; wvar' sigenergy' wtropy wtropy1 se' Ea Ed];
-     %trainFeatures = [trainFeatures; wvar' Ea Ed sigenergy'];
-    %trainFeatures = [trainFeatures; wvar' sigenergy' Ed wp wtropy];
-    %trainFeatures = [trainFeatures; arcoefs se wvar' wtropy];
-     trainFeatures = [trainFeatures; arcoefs se wvar'];
-    %trainFeatures = [trainFeatures; absvalue] ;
+
+%function [trainFeatures, testFeatures,featureIndices] = featureExtraction(trainData,testData,T,level)
+testFeatures = [];
+for idx =1:size(testData1,1)
+    x1 = testData1(idx,:);
+    x1 = detrend(x1,0);
+    arcoefs = blockAR(x1,AROrder,T);
+    se = shannonEntropy(x1,T,level);
+    %[cp,rh] = leaders(x1,T);
+    wvar = modwtvar(modwt(x1,'haar'),'haar');
+    sep = pentropy(x1,T);
+    ifq=  instfreq(x1,T);
+   % testFeatures = [testFeatures;arcoefs se wvar' sep'];
+    testFeatures = [testFeatures;arcoefs se wvar'];
+    %testFeatures = [testFeatures; wvar'];%#ok<AGROW>
+
 end
 
+%allFeatures=featureExtract(TrainTestData, T, AROrder, level);
+%trainFeatures = featureExtract(trainData, T, level);
+%testFeatures = featureExtract(testData, T, level);
 featureindices = struct();
-featureindices.SpectralEntropy = 1:32;
-startidx = 33;
-endidx = startidx+11;
+% 4*8
+featureindices.ARfeatures = 1:4;
+startidx = 5;
+endidx = 5+(1*1)-1;
+featureindices.SEfeatures = startidx:endidx;
+startidx = endidx+1;
+endidx = startidx+12;
 featureindices.WVARfeatures = startidx:endidx;
 end
 
+%%%function [features] = featureExtract(data, T, AROrder,level)
+%function [features] = featureExtract(data, T, level) features = [];
+  %  for i =1:size(data,1)
+    
+       %  x = data(i,:);
+       %  x = detrend(x,0);
+      %   y = buffer(x,T);
+       %  windows = round(numel(x) / T);
+       %  
+       %  length = size(y,2);
+    
+       %  ARCoeffient = zeros(AROrder,length);
+        % entropy = zeros(2^level,length);
+        %pulse = zeros(1,length);
+        %rate = zeros(1,length);
+    
+        %j = 1;
+        % j = 1;
+        % while j < length + 1
+            % temp =  arburg(y(:,j),AROrder);
+            % ARCoeffient(:,j) = temp(2:end);
+            
+            % packetTransform = modwpt(y(:,j),level);
+            %t = sum(packetTransform.^2,2);
+             % t = sum(packetTransform.^2,2);
+             %z = packetTransform.^2./t;
+             % z = packetTransform.^2./t;
+             %entropy(:,j) = -sum(z.*log(z+eps),2);
+            
+           % [~,h,tmp] = dwtleader(y(:,j));
+            %pulse(j) = tmp(2);
+            %rate(j) = range(h);
+            % j = j + 1;
+       % end
+    
+  
+        %%ARCoeffient = reshape(ARCoeffient,AROrder * windows,1);
+        %entropy = reshape(entropy,2^level * windows,1);
+        %variance = modwtvar(modwt(x,'db2'),'db2');
+        
+        %features = [features; ARCoeffient' entropy' pulse rate variance'];
+        %features = [features; ARCoeffient' entropy' variance'];
+        %features = [features; ARCoeffient' variance'];
+        %features = [features; entropy' variance'];   
+   %%% end%%%
 
-function wp = wpe(x,numbuffer,level)
-numwindows = round(numel(x)/numbuffer);
-y = buffer(x,numbuffer);
-wp = zeros(2^level,size(y,2));
-for kk = 1:size(y,2)
-    wpt = modwpt(y(:,kk),level);
-    wp(:,kk) = sum(wpt.^2,2);
-end
-wp = reshape(wp,2^level*numwindows,1);
-wp = wp';
-end
+ %end
+ 
+ 
 function se = shannonEntropy(x,numbuffer,level)
 numwindows = round(numel(x)/numbuffer);
 y = buffer(x,numbuffer);
@@ -87,25 +101,16 @@ se = reshape(se,2^level*numwindows,1);
 se = se';
 end
 
+
 function arcfs = blockAR(x,order,numbuffer)
 numwindows = round(numel(x)/numbuffer);
 y = buffer(x,numbuffer);
 arcfs = zeros(order,size(y,2));
 for kk = 1:size(y,2)
-    artmp =  arburg(y(:,kk),order);
+    %artmp =  arburg(y(:,kk),order);
+    artmp =  armcov(y(:,kk),order);
     arcfs(:,kk) = artmp(2:end);
 end
 arcfs = reshape(arcfs,order*numwindows,1);
 arcfs = arcfs';
-end
-
-function le=leaders(x,numbuffer)
-y = buffer(x,numbuffer);
-le = zeros(1,size(y,3));
-for kk = 1:size(y,3)
-      le = wentropy(y(:,kk),'log energy');
-  %  [~,h,cptmp] = dwtleader(y(:,kk));
-  %  cp(kk) = cptmp(2);
-  % rh(kk) = range(h);
-end
 end
